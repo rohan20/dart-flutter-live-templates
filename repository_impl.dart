3f7504972404e8f1474e9b6363551037e85948e9
Rohan Taneja <rohan20@users.noreply.github.com>
2021-04-18 23:42:24 +0200

    NEW-TEMPLATE: repository impl

---
+++ repository_impl.dart
@@ -0,0 +1,13 @@
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:dartz/dartz.dart';
+
+class $FEATURE_NAME$RepositoryImpl extends $FEATURE_NAME$Repository {
+      final $FEATURE_NAME$RemoteDataSource remoteDataSource;
+      final $FEATURE_NAME$LocalDataSource localDataSource;
+
+    $FEATURE_NAME$RepositoryImpl(this.remoteDataSource, this.localDataSource);
+
+    Future<Either<Failure, $FEATURE_NAME$>> get$FEATURE_NAME$() {
+        
+    }
+}
