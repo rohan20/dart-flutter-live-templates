58165232ae8c2891532e1c7a147d395760914eef
Rohan Taneja <rohan20@users.noreply.github.com>
2021-04-18 23:39:18 +0200

    NEW-TEMPLATE: remote data source impl

---
+++ remote_data_source_impl.dart
@@ -0,0 +1,11 @@
+import 'package:dio/dio.dart';
+
+class $FEATURE_NAME$RemoteDataSourceImpl extends $FEATURE_NAME$RemoteDataSource {
+    final Dio dio;
+    
+    $FEATURE_NAME$RemoteDataSourceImpl(this.dio);
+    
+    Future<String> get$FEATURE_NAME$() {
+        
+    }
+}
